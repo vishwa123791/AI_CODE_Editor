c7f6ce2bf523d3f3f702838077c0ffdedec92f86
Vishwa <vishwanathakunchur@gmail.com>
2026-06-18 14:46:37 +0530

    Delete src directory

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const authRoutes = require('./authRoutes');
-
-router.use('/auth', authRoutes);
-
-module.exports = router;
--- src/utils/ApiError.js
+++
@@ -1,9 +0,0 @@
-class ApiError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-    this.isOperational = true;
-  }
-}
-
-module.exports = ApiError;
